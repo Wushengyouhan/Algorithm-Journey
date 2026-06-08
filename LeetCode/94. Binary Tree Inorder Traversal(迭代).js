b084e90d0b583ad4c7ba5e260f46d39819f468c1
Wushengyouhan <840835067@qq.com>
2026-06-08 10:15:34 +0800

    feat: 94迭代

---
+++ LeetCode/94. Binary Tree Inorder Traversal(迭代).js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let res = [];
+  const stack = [];
+  let curr = root;
+  while (curr || stack.length > 0) {
+    if (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    } else {
+      const node = stack.pop();
+      res.push(node.val);
+      curr = node.right;
+    }
+  }
+
+  return res;
+};
